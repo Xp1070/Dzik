655fa0058dc418c9731d4b78fb47f392f5eeff6c
Xp_107 <126953420+Xp1070@users.noreply.github.com>
2024-10-24 10:17:14 +0200

    Commit 2
    Game is working

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 let i = 1;
+let punkty = 0;
+let czyKliknieto = false;
+let returnMaksa = false;
 function changeNumber()
 {
     let liczba = document.getElementById("id");
     liczba.innerText = i;
     i++;
+    czyKliknieto = false;
 }
 let temp = setInterval(changeNumber, 1000);
 function czySiedemWLiczbie()
 {
-    for(let i = 0; i < liczba.value.length; i++)
+    let liczba = document.getElementById("id").innerText;
+    returnMaksa = false;
+    for(let i = 0; i < liczba.length; i++)
     {
-        if(liczba.value[i] == "7")
+        if(liczba[i] == "7")
         {
-            
+            returnMaksa = true;
         }
     }
 }
 function czyDzik()
 {
     let liczba = document.getElementById("id");
-    if(parseInt(liczba.value) % 7 == 0)
+    let punktyP = document.getElementById("punkty");
+    czySiedemWLiczbie();
+    console.log(returnMaksa);
+    if(parseInt(liczba.innerText) % 7 == 0 || returnMaksa == 1)
     {
-
+        if(!czyKliknieto)
+        {
+            punkty++;
+            punktyP.innerText = punkty
+            console.log(punkty);
+            czyKliknieto = true;
+        }
     }
 }
